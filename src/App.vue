0c864d2fba4403b70735df19573c3892a721b957
李世超 <1286439347@qq.com>
2018-12-14 16:53:06 +0800

    菜单

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <!-- <router-view/> -->
-    <!-- <br>
+    <div id="app">
+        <!-- <router-view/> -->
+        <!-- <br>
     <c-button name="添加一列" type="primary" @click="addList"></c-button>
     <c-button name="显示loading" type="primary" @click="loading = !loading"></c-button>
     <br>
@@ -29,110 +29,116 @@
       <br>
       <c-NavTop :props="defaultProps"></c-NavTop>
     </div>-->
-    <c-LeftNav></c-LeftNav>
-  </div>
+        <span @click="isFold = true">展开</span>
+        <span @click="isFold = false">收起</span>
+        <c-LeftNav :isFold="isFold" @menuClick="menuClick"></c-LeftNav>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      defaultProps: {
-        label: "methodName",
-        value: "methodNameId"
-      },
-      loading: false,
-      butCode: "<c-button></c-button>",
-      butCode1: '<c-button name="主要按钮" type="primary"></c-button>',
-      butCode2: '<c-button name="成功按钮" type="success"></c-button>',
-      butCode3: '<c-button name="信息按钮" type="info"></c-button>',
-      butCode4: '<c-button name="警告按钮" type="warning"></c-button>',
-      butCode5: '<c-button name="危险按钮" type="danger"></c-button>',
-      navTopData: [
-        { methodName: "首页" },
-        { methodName: "组织管理" },
-        { methodName: "用户管理" },
-        { methodName: "资源管理" },
-        { methodName: "财务管理" },
-        { methodName: "内容管理" }
-      ],
-      tableData: [
-        {
-          yy: "空空如也",
-          dy: "变形金刚",
-          mj: "权利的游戏",
-          dm: "斗罗大陆",
-          ty: "足球"
-        },
-        {
-          yy: "桃花朵朵开",
-          dy: "蜘蛛侠",
-          mj: "行尸走肉",
-          dm: "海贼王",
-          ty: "篮球"
-        },
-        {
-          yy: "依恋",
-          dy: "黑豹",
-          mj: "西部世界",
-          dm: "镇魂街",
-          ty: "羽毛球"
-        }
-      ]
-    };
-  },
-  methods: {
-    delList(v) {
-      this.tableData.splice(v, 1);
+    name: "App",
+    data() {
+        return {
+            isFold: false,
+            defaultProps: {
+                label: "methodName",
+                value: "methodNameId"
+            },
+            loading: false,
+            butCode: "<c-button></c-button>",
+            butCode1: '<c-button name="主要按钮" type="primary"></c-button>',
+            butCode2: '<c-button name="成功按钮" type="success"></c-button>',
+            butCode3: '<c-button name="信息按钮" type="info"></c-button>',
+            butCode4: '<c-button name="警告按钮" type="warning"></c-button>',
+            butCode5: '<c-button name="危险按钮" type="danger"></c-button>',
+            navTopData: [
+                { methodName: "首页" },
+                { methodName: "组织管理" },
+                { methodName: "用户管理" },
+                { methodName: "资源管理" },
+                { methodName: "财务管理" },
+                { methodName: "内容管理" }
+            ],
+            tableData: [
+                {
+                    yy: "空空如也",
+                    dy: "变形金刚",
+                    mj: "权利的游戏",
+                    dm: "斗罗大陆",
+                    ty: "足球"
+                },
+                {
+                    yy: "桃花朵朵开",
+                    dy: "蜘蛛侠",
+                    mj: "行尸走肉",
+                    dm: "海贼王",
+                    ty: "篮球"
+                },
+                {
+                    yy: "依恋",
+                    dy: "黑豹",
+                    mj: "西部世界",
+                    dm: "镇魂街",
+                    ty: "羽毛球"
+                }
+            ]
+        };
     },
-    addList() {
-      let num = parseInt(Math.random() * this.tableData.length + 1);
-      this.tableData.push(this.tableData[num - 1]);
-    }
-  },
-  mounted() {}
+    methods: {
+        delList(v) {
+            this.tableData.splice(v, 1);
+        },
+        addList() {
+            let num = parseInt(Math.random() * this.tableData.length + 1);
+            this.tableData.push(this.tableData[num - 1]);
+        },
+        menuClick(a,b,c){
+            console.log(a,b,c)
+        }
+    },
+    mounted() {}
 };
 </script>
 
 <style scoped>
 * {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 #app {
-  /* width: 1200px; */
-  height: 100%;
-  margin: auto;
+    /* width: 1200px; */
+    height: 100%;
+    margin: auto;
 }
 
 h1,
 h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+    color: #42b983;
 }
 
 input,
 button,
 select,
 textarea {
-  outline: none;
+    outline: none;
 }
 
 textarea {
-  resize: none;
+    resize: none;
 }
 </style>
--- src/components/Cui/LeftNav.vue
+++ src/components/Cui/LeftNav.vue
@@ -1,247 +1,407 @@
 <template>
-  <div class="nav_box">
-    <ul
-      class="cursor nav_content_ul fl"
-      :style="{'width': width+'px'}"
-      v-for="(time_ul,F)  in childrenNum+1"
-      :key="time_ul"
+    <div
+        class="nav_box"
+        :style="{background: BGColor, color: initTcolor,top: menuTop}"
+        @mouseleave="mouseleaveNav"
     >
-      <li
-        class="nav-list"
-        :style="{'height':'50px','line-height':'50px'}"
-        v-for="(time,W) in  childrenData[F]"
-        :key="time.id"
-        @mouseenter="aaa(F,W,time.children)"
-      >
-        <span class="fl">{{time.methodName}}</span>
-        <span class="fr">
-          <svg
-            t="1544706884297"
-            class="icon"
-            style
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            p-id="1226"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="18"
-            height="18"
-          >
-            <path
-              d="M312.888889 995.555556c-17.066667 0-28.444444-5.688889-39.822222-17.066667-22.755556-22.755556-17.066667-56.888889 5.688889-79.644445l364.088888-329.955555c11.377778-11.377778 17.066667-22.755556 17.066667-34.133333 0-11.377778-5.688889-22.755556-17.066667-34.133334L273.066667 187.733333c-22.755556-22.755556-28.444444-56.888889-5.688889-79.644444 22.755556-22.755556 56.888889-28.444444 79.644444-5.688889l364.088889 312.888889c34.133333 28.444444 56.888889 73.955556 56.888889 119.466667s-17.066667 85.333333-51.2 119.466666l-364.088889 329.955556c-11.377778 5.688889-28.444444 11.377778-39.822222 11.377778z"
-              p-id="1227"
-            ></path>
-          </svg>
-        </span>
-      </li>
-    </ul>
-    <!-- {{childrenData}} -->
-    <div class="dock_bottom">欢迎咨询10086</div>
-  </div>
+        <ul
+            class="nav_content_ul boxSizing fl"
+            :style="{'width': menuW+'px'}"
+            v-for="(time_ul,F)  in childrenNum+1"
+            :key="time_ul[props.value]"
+        >
+            <li
+                class="cursor nav_list boxSizing text_overflow"
+                v-for="(time,W) in  childrenData[F]"
+                :key="time[props.value]"
+                :style="{color: W == num[F] ? ATcolor :'',height: menuH+'px', 'line-height':  menuH+'px'}"
+                @mouseenter="mouseenterLi(F,W,time[props.children])"
+                @click="clickChilMenu(F,W,time[props.children],time)"
+            >
+                <span class="icons fl">
+                    <img
+                        :src="time.url"
+                        alt=""
+                        srcset=""
+                    >
+                </span>
+                <span>
+                    <span
+                        :style="{width: menuW-100 +'px' }"
+                        class="menuTitle text_overflow"
+                        v-show="isFold"
+                    >
+                        {{time[props.label]}}
+                    </span>
+                </span>
+
+                <span v-show="isFold && time[props.children].length">
+                    <svg
+                        :style="{'margin-top': menuH - 35 +'px'}"
+                        t="1544706884297"
+                        class="icon fr"
+                        fill="#ccc"
+                        viewBox="0 0 1024 1024"
+                        version="1.1"
+                        p-id="1226"
+                        width="20"
+                        height="20"
+                    >
+                        <path
+                            d="M312.888889 995.555556c-17.066667 0-28.444444-5.688889-39.822222-17.066667-22.755556-22.755556-17.066667-56.888889 5.688889-79.644445l364.088888-329.955555c11.377778-11.377778 17.066667-22.755556 17.066667-34.133333 0-11.377778-5.688889-22.755556-17.066667-34.133334L273.066667 187.733333c-22.755556-22.755556-28.444444-56.888889-5.688889-79.644444 22.755556-22.755556 56.888889-28.444444 79.644444-5.688889l364.088889 312.888889c34.133333 28.444444 56.888889 73.955556 56.888889 119.466667s-17.066667 85.333333-51.2 119.466666l-364.088889 329.955556c-11.377778 5.688889-28.444444 11.377778-39.822222 11.377778z"
+                            p-id="1227"
+                        ></path>
+                    </svg>
+                </span>
+            </li>
+        </ul>
+        <div class="clear"></div>
+        <div
+            class="dock_bottom"
+            :style="{'width': menuW+'px'}"
+            v-show="isFold"
+        >欢迎咨询10086</div>
+    </div>
 </template>
 <script>
 export default {
-  name: "LeftNav",
-  components: {},
-  props: {
-    isFixedNav: {
-      type: [Boolean],
-      default: false
-    },
-    width: {
-      type: [Number],
-      default: 240
-    },
-    height: {
-      type: [Number],
-      default: 62
-    },
-    navTopData: {
-      type: Array,
-      default: function() {
-        return [
-          { methodName: "首页", id: 1, children: [] },
-          {
-            methodName: "组织管理",
-            id: 2,
-            children: [
-              {
-                methodName: "组织管理-1",
-                id: 22,
-                children: [
-                  {
-                    methodName: "组织管理-1-1",
-                    id: 222,
-                    children: [
-                      { methodName: "组织管理-1-1-1", id: 2222, children: [] }
-                    ]
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            methodName: "用户管理",
-            id: 3,
-            children: [
-              {
-                methodName: "用户管理-1",
-                id: 33,
-                children: [
-                  { methodName: "用户管理-1-1", id: 333, children: [] }
-                ]
-              }
-            ]
-          },
-          {
-            methodName: "资源管理",
-            id: 4,
-            children: [{ methodName: "资源管理-1", id: 44, children: [] }]
-          },
-          {
-            methodName: "财务管理",
-            id: 5,
-            children: [{ methodName: "财务管理-1", id: 55, children: [] }]
-          },
-          {
-            methodName: "内容管理",
-            id: 6,
-            children: [{ methodName: "内容管理-1", id: 66, children: [] }]
-          }
-        ];
-      }
-    },
-    backgroundColor: {
-      type: [String],
-      default: "#555"
-    },
-    textColor: {
-      type: [String],
-      default: "#fff"
-    },
-    activeTextColor: {
-      type: [String],
-      default: "#ffd04b"
-    },
-    hoverBackground: {
-      type: [String],
-      default: "#444"
-    },
-    activeIndex: {
-      type: [Number],
-      default: 0
-    },
-    userName: {
-      type: [String],
-      default: "userName"
-    },
-    userImg: {
-      type: [String],
-      default:
-        "http://f.elnplus.com/data/images/user/photo/1347379288888909_f5f689bad0d54e7b9872146765484335.jpg"
-    },
-    isShowUser: {
-      type: [Boolean],
-      default: true
-    },
-    logoImg: {
-      type: [String],
-      default: "http://www.logoaplus.com/themes/logoplus/img/logo-darken.svg"
-    },
-    logoImgWidth: {
-      type: [Number],
-      default: 240
-    },
-    isShowLogo: {
-      type: [Boolean],
-      default: true
-    }
-  },
-  data() {
-    return {
-      childrenData: [],
-      childrenNum: 0,
-      num: 0
-    };
-  },
-  methods: {
-    aaa(F, W, chil) {
-      if (chil.length) {
-        this.childrenNum = F + 1;
-      } else {
-        if (F === 0) {
-          this.childrenNum = 0;
-          return;
+    name: "LeftNav",
+    components: {},
+    props: {
+        menuH: {
+            type: [Number],
+            default: 50
+        },
+        // 默认key值
+        props: {
+            type: Object,
+            default: function() {
+                return {
+                    label: "name",
+                    value: "id",
+                    url: "url",
+                    children: "children"
+                };
+            }
+        },
+        // 菜单顶部距离
+        menuTop: {
+            type: [String],
+            default: "50px"
+        },
+        // 展开收起菜单
+        isFold: {
+            type: [Boolean],
+            default: false
+        },
+        // 菜单宽度
+        menuW: {
+            type: [Number],
+            default: 200
+        },
+        // 菜单数据
+        navTopData: {
+            type: Array,
+            default: function() {
+                return [
+                    {
+                        name: "首页",
+                        id: "1",
+                        url: "https://cn.vuejs.org/images/logo.png",
+                        children: []
+                    },
+                    {
+                        name: "组织管理",
+                        id: "2",
+                        url: "https://cn.vuejs.org/images/logo.png",
+                        children: [
+                            {
+                                name: "组织管理2-1",
+                                id: "2-1",
+                                url: "https://cn.vuejs.org/images/logo.png",
+                                children: [
+                                    {
+                                        name: "组织管理2-1-1",
+                                        id: "2-1-1",
+                                        url:
+                                            "https://cn.vuejs.org/images/logo.png",
+                                        children: [
+                                            {
+                                                name: "组织管理2-1-1-1",
+                                                id: "2-1-1-1",
+                                                url:
+                                                    "https://cn.vuejs.org/images/logo.png",
+                                                children: []
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                name: "组织管理2-2",
+                                id: "2-2",
+                                url: "https://cn.vuejs.org/images/logo.png",
+                                children: [
+                                    {
+                                        name: "组织管理2-2-1",
+                                        id: "2-2-1",
+                                        url:
+                                            "https://cn.vuejs.org/images/logo.png",
+                                        children: [
+                                            {
+                                                name: "组织管理2-2-1-1",
+                                                id: "2-2-1-1",
+                                                url:
+                                                    "https://cn.vuejs.org/images/logo.png",
+                                                children: []
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: "用户管理",
+                        id: "3",
+                        url: "https://cn.vuejs.org/images/logo.png",
+                        children: [
+                            {
+                                name: "用户管理3-1",
+                                id: "3-1",
+                                url: "https://cn.vuejs.org/images/logo.png",
+                                children: [
+                                    {
+                                        name: "用户管理3-1-1",
+                                        id: "3-1-1",
+                                        url:
+                                            "https://cn.vuejs.org/images/logo.png",
+                                        children: []
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: "资源管理",
+                        id: "4",
+                        url: "https://cn.vuejs.org/images/logo.png",
+                        children: [
+                            {
+                                name: "资源管理4-1",
+                                id: "3-1",
+                                url: "https://cn.vuejs.org/images/logo.png",
+                                children: []
+                            }
+                        ]
+                    },
+                    {
+                        name: "财务管理",
+                        id: "5",
+                        url: "https://cn.vuejs.org/images/logo.png",
+                        children: [
+                            {
+                                name: "财务管理5-1",
+                                id: "5-1",
+                                url: "https://cn.vuejs.org/images/logo.png",
+                                children: []
+                            }
+                        ]
+                    },
+                    {
+                        name: "内容管理",
+                        id: "6",
+                        url: "https://cn.vuejs.org/images/logo.png",
+                        children: [
+                            {
+                                name: "内容管理6-1",
+                                id: "6-1",
+                                url: "https://cn.vuejs.org/images/logo.png",
+                                children: []
+                            }
+                        ]
+                    }
+                ];
+            }
+        },
+        // 菜单背景色
+        BGColor: {
+            type: [String],
+            default: "#7c97ac"
+        },
+        // 默认文字颜色
+        initTcolor: {
+            type: [String],
+            default: "#fff"
+        },
+        //文字选中颜色
+        ATcolor: {
+            type: [String],
+            default: "aquamarine"
+        },
+        // 文字选中背景--未使用
+        ATbackground: {
+            type: [String],
+            default: ""
+        },
+        // 当前选择位置--未使用
+        activeIndex: {
+            type: [Number],
+            default: 0
+        },
+        // false为点击展开true为鼠标移入展开
+        isClickHover: {
+            type: [Boolean],
+            default: false
         }
-        this.childrenNum = F;
-      }
-      this.$set(this.childrenData, F + 1, chil);
-      console.log(F, W, chil);
+    },
+    data() {
+        return {
+            childrenData: [],
+            childrenNum: 0,
+            num: [],
+            maxBoxW: ""
+        };
+    },
+    computed: {},
+    methods: {
+        // 点击菜单
+        clickChilMenu(F, W, chil, time) {
+            this.$set(this.num, F, W);
+            if (this.isFold === false || this.isClickHover == true) return;
+            if (chil.length) {
+                this.childrenNum = F + 1;
+                this.maxBoxW.style.width =
+                    this.menuW * (this.childrenNum + 1) + "px";
+            } else {
+                if (F === 0) {
+                    this.childrenNum = 0;
+                    this.maxBoxW.style.width = this.menuW + "px";
+                }
+                this.childrenNum = F;
+            }
+            this.num.splice(F + 1, 1);
+            this.$set(this.childrenData, F + 1, chil);
+            this.$emit("menuClick", F, W, time);
+        },
+        // 鼠标移入
+        mouseenterLi(F, W, chil) {
+            this.$set(this.num, F, W);
+            if (this.isFold === false || this.isClickHover == false) return;
+            if (chil.length) {
+                this.childrenNum = F + 1;
+                this.maxBoxW.style.width =
+                    this.menuW * (this.childrenNum + 1) + "px";
+            } else {
+                if (F === 0) {
+                    this.childrenNum = 0;
+                    this.maxBoxW.style.width = this.menuW + "px";
+                }
+                this.childrenNum = F;
+            }
+            this.num.splice(F + 1, 1);
+            this.$set(this.childrenData, F + 1, chil);
+        },
+        // 鼠标移出
+        mouseleaveNav() {
+            if (this.isFold === false) return;
+            // this.maxBoxW.style.width = this.menuW + "px";
+            // this.num = [];
+        }
+    },
+    created() {
+        this.childrenData.push(this.navTopData);
+    },
+    mounted() {
+        this.maxBoxW = document.getElementsByClassName("nav_box")[0];
+        this.maxBoxW.style.width = this.isFold ? this.menuW + "px" : "60px";
+    },
+    watch: {
+        isFold: function(o, v) {
+            this.$nextTick(() => {
+                this.maxBoxW.style.width = o ? this.menuW + "px" : "60px";
+            });
+        }
     }
-  },
-  created() {
-    this.childrenData.push(this.navTopData);
-  },
-  mounted() {},
-  watch: {}
 };
 </script>
 <style scoped>
 * {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 html,
 body {
-  height: 100%;
-  width: 100%;
+    height: 100%;
+    width: 100%;
 }
-.w50 {
-  width: 49%;
+.clear {
+    clear: both;
+}
+.icons {
+}
+.icons img {
+    height: 30px;
+}
+.menuTitle {
+    display: inline-block;
+    width: 80px;
+    /* margin-left: 10px; */
+    padding-left: 10px;
 }
 li {
-  list-style-type: none;
+    list-style-type: none;
 }
 img {
-  vertical-align: middle;
+    vertical-align: middle;
 }
 textarea {
-  resize: none;
-}
-.icon {
+    resize: none;
 }
 .fl {
-  float: left;
+    float: left;
 }
 .fr {
-  float: right;
+    float: right;
 }
 .cursor {
-  cursor: pointer;
+    cursor: pointer;
 }
-.nav-list {
-  padding: 0 20px;
+.nav_list {
+    height: 70px;
+    line-height: 70px;
+    padding: 0 15px;
+    border-bottom: 1px solid #ccc;
+    transition: all 0.4s;
 }
-.nav-list:hover {
-  background: #ccc;
-  color: aquamarine;
+.text_overflow {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
 }
 .nav_box {
-  /* background: #ccc; */
-  height: 100%;
-  position: fixed;
-  /* top: 50px; */
-  position: relative;
+    height: 100%;
+    display: inline-block;
+    position: fixed;
+    left: 0;
+    overflow: hidden;
+    transition: all 0.4s;
+    color: #fff;
 }
 .dock_bottom {
-  width: 100%;
-  text-align: center;
-  position: absolute;
-  bottom: 25px;
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    bottom: 25px;
 }
 .nav_content_ul {
-  border: 1px solid red;
-  height: 100%;
+    height: 100%;
+    border-right: 1px solid #ccc;
+}
+.boxSizing {
+    box-sizing: border-box;
+    -moz-box-sizing: border-box; /* Firefox */
+    -webkit-box-sizing: border-box; /* Safari */
 }
 </style>
 
